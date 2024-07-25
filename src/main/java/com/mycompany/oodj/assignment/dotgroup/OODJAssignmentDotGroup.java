dd4532a6200d22a5bb8a232d7b9413f47998545d
Qayyum Yazid <purrnama@proton.me>
2024-07-25 13:26:30 +0800

    initial commit

---
+++ src/main/java/com/mycompany/oodj/assignment/dotgroup/OODJAssignmentDotGroup.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oodj.assignment.dotgroup;
+
+/**
+ *
+ * @author purrnama
+ */
+public class OODJAssignmentDotGroup {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
